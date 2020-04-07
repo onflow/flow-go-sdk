5bdfee6ecec9a68dc92dc2e05432ce4ad04e0f56
Peter Siemens <peterjsiemens@gmail.com>
2020-04-07 15:55:39 -0700

    Add flow.ZeroID variable

--- flow.go
+++ flow.go
@@ -3,6 +3,8 @@ package flow
 // Identifier represents a 32-byte unique identifier for an entity.
 type Identifier [32]byte
 
+var ZeroID = Identifier{}
+
 func (i Identifier) Bytes() []byte {
 	return i[:]
 }
