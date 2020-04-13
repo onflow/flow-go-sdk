615aac4fdc32540f6069fb24d92c1cc55f061160
Peter Siemens <peterjsiemens@gmail.com>
2020-04-13 12:51:58 -0700

    Update protobuf conversion functions

--- client/convert/protobuf_test.go
+++ client/convert/protobuf_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/dapperlabs/flow-go-sdk"
 	"github.com/dapperlabs/flow-go-sdk/client/convert"
-	"github.com/dapperlabs/flow-go-sdk/crypto"
+	"github.com/dapperlabs/flow-go-sdk/test"
 )
 
 var (
@@ -23,28 +23,7 @@ func init() {
 }
 
 func TestConvert_Transaction(t *testing.T) {
-	txA := flow.NewTransaction().
-		SetScript([]byte(`transaction { execute { log("Hello, World!") } }`)).
-		SetReferenceBlockID(flow.Identifier{0x01, 0x02}).
-		SetGasLimit(42).
-		SetProposalKey(AddressA, 1, 42).
-		AddAuthorizer(AddressA, 1, 2).
-		SetPayer(AddressB, 1)
-
-	err := txA.SignPayload(AddressA, 1, crypto.MockSigner([]byte{1}))
-	if err != nil {
-		panic(err)
-	}
-
-	err = txA.SignPayload(AddressA, 2, crypto.MockSigner([]byte{2}))
-	if err != nil {
-		panic(err)
-	}
-
-	err = txA.SignContainer(AddressB, 1, crypto.MockSigner([]byte{1}))
-	if err != nil {
-		panic(err)
-	}
+	txA := test.TransactionGenerator().New()
 
 	msg := convert.TransactionToMessage(*txA)
 
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/dapperlabs/cadence v0.0.0-20200401151421-7c78a2da20a6
 	github.com/dapperlabs/flow-go v0.3.2-0.20200331201607-7630da6300eb
 	github.com/dapperlabs/flow-go/crypto v0.3.2-0.20200331201607-7630da6300eb
-	github.com/dapperlabs/flow/protobuf/go/flow v0.0.0-20200409042800-60d3bad7767d
+	github.com/dapperlabs/flow/protobuf/go/flow v0.0.0-20200413194623-ccc4f4197409
 	github.com/lithammer/dedent v1.1.0
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381 // indirect
 	github.com/magiconair/properties v1.8.1
