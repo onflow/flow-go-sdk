6e2622e4b01f0cb6b10636114041bce0ba4cdb8d
Peter Siemens <peterjsiemens@gmail.com>
2020-04-15 11:23:59 -0700

    Add Collection and CollectionGuarantee models

---
+++ collection.go
@@ -0,0 +1,36 @@
+package flow
+
+import (
+	"github.com/dapperlabs/flow-go/model/hash"
+)
+
+// A Collection is a list of transactions bundled together for inclusion in a block.
+type Collection struct {
+	TransactionIDs []Identifier
+}
+
+// ID returns the canonical SHA3-256 hash of this collection.
+func (c Collection) ID() Identifier {
+	return HashToID(hash.DefaultHasher.ComputeHash(c.Encode()))
+}
+
+// Encode returns the canonical encoding of this collection.
+func (c Collection) Encode() []byte {
+	transactionIDs := make([][]byte, len(c.TransactionIDs))
+	for i, id := range c.TransactionIDs {
+		transactionIDs[i] = id.Bytes()
+	}
+
+	temp := struct {
+		TransactionIDS [][]byte
+	}{
+		TransactionIDS: transactionIDs,
+	}
+
+	return DefaultEncoder.MustEncode(&temp)
+}
+
+// A CollectionGuarantee is an attestation signed by the nodes that have guaranteed a collection.
+type CollectionGuarantee struct {
+	CollectionID Identifier
+}
