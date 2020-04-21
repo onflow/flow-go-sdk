3007d52198329ff9ee2e4de5f508ea24dde2ff56
Peter Siemens <peterjsiemens@gmail.com>
2020-04-21 00:24:25 -0700

    Add godoc comments for Block, BlockHeader, BlockPayload

--- block.go
+++ block.go
@@ -1,16 +1,21 @@
 package flow
 
+// A Block is a set of state mutations applied to the Flow blockchain.
 type Block struct {
 	BlockHeader
 	BlockPayload
 }
 
+// A BlockHeader is a summary of a full block.
 type BlockHeader struct {
 	ID       Identifier
 	ParentID Identifier
 	Height   uint64
 }
 
+// A BlockPayload is the full contents of a block.
+//
+// A payload contains the collection guarantees and seals contained within a block.
 type BlockPayload struct {
 	Guarantees []*CollectionGuarantee
 	Seals      []*BlockSeal
