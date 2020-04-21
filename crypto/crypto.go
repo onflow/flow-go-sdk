88245f1af7547bf63f2d91657dfb22478759213d
Peter Siemens <peterjsiemens@gmail.com>
2020-04-21 08:14:38 -0700

    Crypto: move hash functions to separate file, add godoc comments

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -225,44 +225,6 @@ func DecodePublicKeyHex(sigAlgo SignatureAlgorithm, s string) (PublicKey, error)
 	return DecodePublicKey(sigAlgo, b)
 }
 
-type Hasher = hash.Hasher
-type Hash = hash.Hash
-
-func NewHasher(algo HashAlgorithm) (Hasher, error) {
-	switch algo {
-	case SHA2_256:
-		return NewSHA2_256(), nil
-	case SHA2_384:
-		return NewSHA2_384(), nil
-	case SHA3_256:
-		return NewSHA3_256(), nil
-	case SHA3_384:
-		return NewSHA3_384(), nil
-	default:
-		return nil, fmt.Errorf("invalid hash algorithm %s", algo)
-	}
-}
-
-// NewSHA2_256 returns a new instance of SHA2-256 hasher.
-func NewSHA2_256() Hasher {
-	return hash.NewSHA2_256()
-}
-
-// NewSHA2_384 returns a new instance of SHA2-384 hasher.
-func NewSHA2_384() Hasher {
-	return hash.NewSHA2_384()
-}
-
-// NewSHA3_256 returns a new instance of SHA3-256 hasher.
-func NewSHA3_256() Hasher {
-	return hash.NewSHA3_256()
-}
-
-// NewSHA3_384 returns a new instance of SHA3-384 hasher.
-func NewSHA3_384() Hasher {
-	return hash.NewSHA3_384()
-}
-
 // CompatibleAlgorithms returns true if the signature and hash algorithms are compatible.
 func CompatibleAlgorithms(sigAlgo SignatureAlgorithm, hashAlgo HashAlgorithm) bool {
 	switch sigAlgo {
