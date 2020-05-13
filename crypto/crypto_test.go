7b6dca1b2c93dec5d1384e9482c0e346c02eb48b
Peter Siemens <peterjsiemens@gmail.com>
2020-05-13 14:37:15 -0700

    crypto: Clean up GeneratePrivateKey tests

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -1,3 +1,21 @@
+/*
+ * Flow Go SDK
+ *
+ * Copyright 2019-2020 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package crypto_test
 
 import (
@@ -11,10 +29,12 @@ import (
 
 func TestGeneratePrivateKey(t *testing.T) {
 	validAlgo := crypto.ECDSA_P256
-	validSeed := make([]byte, validAlgo.MinSeedLength()*2) // make seed that is twice the minimum length
-	for i, _ := range validSeed {
-		validSeed[i] = byte(i)
-	}
+	invalidAlgo := crypto.SignatureAlgorithm(-42)
+
+	emptySeed := makeSeed(0)
+	shortSeed := makeSeed(validAlgo.MinSeedLength() / 2)
+	equalSeed := makeSeed(validAlgo.MinSeedLength())
+	longSeed := makeSeed(validAlgo.MinSeedLength() * 2)
 
 	t.Run("Nil seed", func(t *testing.T) {
 		sk, err := crypto.GeneratePrivateKey(validAlgo, nil)
@@ -23,39 +43,32 @@ func TestGeneratePrivateKey(t *testing.T) {
 	})
 
 	t.Run("Empty seed", func(t *testing.T) {
-		sk, err := crypto.GeneratePrivateKey(validAlgo, []byte{})
+		sk, err := crypto.GeneratePrivateKey(validAlgo, emptySeed)
 		assert.Error(t, err)
 		assert.Equal(t, crypto.PrivateKey{}, sk)
 	})
 
 	t.Run("Seed length too short", func(t *testing.T) {
-		sk, err := crypto.GeneratePrivateKey(validAlgo, []byte("foo"))
+		sk, err := crypto.GeneratePrivateKey(validAlgo, shortSeed)
 		assert.Error(t, err)
 		assert.Equal(t, crypto.PrivateKey{}, sk)
 	})
 
 	t.Run("Seed length exactly equal", func(t *testing.T) {
-		seed := make([]byte, validAlgo.MinSeedLength())
-		for i, _ := range seed {
-			seed[i] = byte(i)
-		}
-
-		sk, err := crypto.GeneratePrivateKey(validAlgo, seed)
+		sk, err := crypto.GeneratePrivateKey(validAlgo, equalSeed)
 		assert.NoError(t, err)
 		assert.NotEqual(t, crypto.PrivateKey{}, sk)
 		assert.Equal(t, validAlgo, sk.Algorithm())
 	})
 
 	t.Run("Invalid signature algorithm", func(t *testing.T) {
-		invalidAlgo := crypto.SignatureAlgorithm(-42)
-
-		sk, err := crypto.GeneratePrivateKey(invalidAlgo, validSeed)
+		sk, err := crypto.GeneratePrivateKey(invalidAlgo, longSeed)
 		assert.Error(t, err)
 		assert.Equal(t, crypto.PrivateKey{}, sk)
 	})
 
 	t.Run("Valid signature algorithm", func(t *testing.T) {
-		sk, err := crypto.GeneratePrivateKey(validAlgo, validSeed)
+		sk, err := crypto.GeneratePrivateKey(validAlgo, longSeed)
 		assert.NoError(t, err)
 		assert.NotEqual(t, crypto.PrivateKey{}, sk)
 		assert.Equal(t, validAlgo, sk.Algorithm())
@@ -67,14 +80,22 @@ func TestGeneratePrivateKey(t *testing.T) {
 		var skA crypto.PrivateKey
 		var err error
 
-		skA, err = crypto.GeneratePrivateKey(validAlgo, validSeed)
+		skA, err = crypto.GeneratePrivateKey(validAlgo, longSeed)
 		require.NoError(t, err)
 
 		for i := 0; i < trials; i++ {
-			skB, err := crypto.GeneratePrivateKey(validAlgo, validSeed)
+			skB, err := crypto.GeneratePrivateKey(validAlgo, longSeed)
 			require.NoError(t, err)
 			assert.Equal(t, skA, skB) // key should be same each time
 			skA = skB
 		}
 	})
+}
+
+func makeSeed(l int) []byte {
+	seed := make([]byte, l)
+	for i, _ := range seed {
+		seed[i] = byte(i)
+	}
+	return seed
 }
