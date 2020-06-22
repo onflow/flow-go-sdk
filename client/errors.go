cd6b4f9b6c5bbd8de7551b342f1082d4632ec8bf
Peter Siemens <peterjsiemens@gmail.com>
2020-06-22 00:02:32 -0700

    client: add client.RPCError type

--- client/errors.go
+++ client/errors.go
@@ -2,12 +2,35 @@ package client
 
 import (
 	"fmt"
+
+	"google.golang.org/grpc/status"
 )
 
 const errorMessagePrefix = "client: "
 
 func errorMessage(format string, a ...interface{}) string {
 	return errorMessagePrefix + fmt.Sprintf(format, a...)
+}
+
+type RPCError struct {
+	GRPCError error
+}
+
+func newRPCError(gRPCErr error) RPCError {
+	return RPCError{GRPCError: gRPCErr}
+}
+
+func (e RPCError) Error() string {
+	return errorMessage(e.GRPCError.Error())
+}
+
+func (e RPCError) Unwrap() error {
+	return e.GRPCError
+}
+
+func (e RPCError) GRPCStatus() *status.Status {
+	s, _ := status.FromError(e.GRPCError)
+	return s
 }
 
 const (
@@ -26,10 +49,6 @@ type EntityToMessageError struct {
 	Err    error
 }
 
-func (e EntityToMessageError) Error() string {
-	return errorMessage("failed to construct protobuf message from %s entity: %s", e.Entity, e.Err.Error())
-}
-
 func newEntityToMessageError(entity string, err error) EntityToMessageError {
 	return EntityToMessageError{
 		Entity: entity,
@@ -37,13 +56,17 @@ func newEntityToMessageError(entity string, err error) EntityToMessageError {
 	}
 }
 
+func (e EntityToMessageError) Error() string {
+	return errorMessage("failed to construct protobuf message from %s entity: %s", e.Entity, e.Err.Error())
+}
+
+func (e EntityToMessageError) Unwrap() error {
+	return e.Err
+}
+
 type MessageToEntityError struct {
 	Entity string
 	Err    error
-}
-
-func (e MessageToEntityError) Error() string {
-	return errorMessage("failed to construct %s entity from protobuf value: %s", e.Entity, e.Err.Error())
 }
 
 func newMessageToEntityError(entity string, err error) MessageToEntityError {
@@ -51,4 +74,12 @@ func newMessageToEntityError(entity string, err error) MessageToEntityError {
 		Entity: entity,
 		Err:    err,
 	}
+}
+
+func (e MessageToEntityError) Error() string {
+	return errorMessage("failed to construct %s entity from protobuf value: %s", e.Entity, e.Err.Error())
+}
+
+func (e MessageToEntityError) Unwrap() error {
+	return e.Err
 }
