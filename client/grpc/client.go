30f6ca131e6e1283ba29526beb25cdc74cfc99b9
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-03-16 12:00:17 +0100

    export handler

--- client/grpc/client.go
+++ client/grpc/client.go
@@ -8,79 +8,79 @@ import (
 )
 
 type Client struct {
-	handler Handler
+	Handler Handler
 }
 
 func (c Client) Ping(ctx context.Context) error {
-	return c.handler.Ping(ctx)
+	return c.Handler.Ping(ctx)
 }
 
 func (c Client) GetLatestBlockHeader(ctx context.Context, isSealed bool) (*flow.BlockHeader, error) {
-	return c.handler.GetLatestBlockHeader(ctx, isSealed)
+	return c.Handler.GetLatestBlockHeader(ctx, isSealed)
 }
 
 func (c Client) GetBlockHeaderByID(ctx context.Context, blockID flow.Identifier) (*flow.BlockHeader, error) {
-	return c.handler.GetBlockHeaderByID(ctx, blockID)
+	return c.Handler.GetBlockHeaderByID(ctx, blockID)
 }
 
 func (c Client) GetBlockHeaderByHeight(ctx context.Context, height uint64) (*flow.BlockHeader, error) {
-	return c.handler.GetBlockHeaderByHeight(ctx, height)
+	return c.Handler.GetBlockHeaderByHeight(ctx, height)
 }
 
 func (c Client) GetLatestBlock(ctx context.Context, isSealed bool) (*flow.Block, error) {
-	return c.handler.GetLatestBlock(ctx, isSealed)
+	return c.Handler.GetLatestBlock(ctx, isSealed)
 }
 
 func (c Client) GetBlockByID(ctx context.Context, blockID flow.Identifier) (*flow.Block, error) {
-	return c.handler.GetBlockByID(ctx, blockID)
+	return c.Handler.GetBlockByID(ctx, blockID)
 }
 
 func (c Client) GetBlockByHeight(ctx context.Context, height uint64) (*flow.Block, error) {
-	return c.handler.GetBlockByHeight(ctx, height)
+	return c.Handler.GetBlockByHeight(ctx, height)
 }
 
 func (c Client) GetCollection(ctx context.Context, colID flow.Identifier) (*flow.Collection, error) {
-	return c.handler.GetCollection(ctx, colID)
+	return c.Handler.GetCollection(ctx, colID)
 }
 
 func (c Client) SendTransaction(ctx context.Context, tx flow.Transaction) error {
-	return c.handler.SendTransaction(ctx, tx)
+	return c.Handler.SendTransaction(ctx, tx)
 }
 
 func (c Client) GetTransaction(ctx context.Context, txID flow.Identifier) (*flow.Transaction, error) {
-	return c.handler.GetTransaction(ctx, txID)
+	return c.Handler.GetTransaction(ctx, txID)
 }
 
 func (c Client) GetTransactionResult(ctx context.Context, txID flow.Identifier) (*flow.TransactionResult, error) {
-	return c.handler.GetTransactionResult(ctx, txID)
+	return c.Handler.GetTransactionResult(ctx, txID)
 }
 
 func (c Client) GetAccount(ctx context.Context, address flow.Address) (*flow.Account, error) {
-	return c.handler.GetAccount(ctx, address)
+	return c.Handler.GetAccount(ctx, address)
 }
 
 func (c Client) GetAccountAtLatestBlock(ctx context.Context, address flow.Address) (*flow.Account, error) {
-	return c.handler.GetAccountAtLatestBlock(ctx, address)
+	return c.Handler.GetAccountAtLatestBlock(ctx, address)
 }
 
 func (c Client) GetAccountAtBlockHeight(ctx context.Context, address flow.Address, blockHeight uint64) (*flow.Account, error) {
-	return c.handler.GetAccountAtBlockHeight(ctx, address, blockHeight)
+	return c.Handler.GetAccountAtBlockHeight(ctx, address, blockHeight)
 }
 
 func (c Client) ExecuteScriptAtLatestBlock(ctx context.Context, script []byte, arguments []cadence.Value) (cadence.Value, error) {
-	return c.handler.ExecuteScriptAtLatestBlock(ctx, script, arguments)
+	return c.Handler.ExecuteScriptAtLatestBlock(ctx, script, arguments)
 }
 
 func (c Client) ExecuteScriptAtBlockID(ctx context.Context, blockID flow.Identifier, script []byte, arguments []cadence.Value) (cadence.Value, error) {
-	return c.handler.ExecuteScriptAtBlockID(ctx, blockID, script, arguments)
+	return c.Handler.ExecuteScriptAtBlockID(ctx, blockID, script, arguments)
 }
 
 func (c Client) ExecuteScriptAtBlockHeight(ctx context.Context, height uint64, script []byte, arguments []cadence.Value) (cadence.Value, error) {
-	return c.handler.ExecuteScriptAtBlockHeight(ctx, height, script, arguments)
+	return c.Handler.ExecuteScriptAtBlockHeight(ctx, height, script, arguments)
 }
 
 func (c Client) GetEventsForHeightRange(ctx context.Context, eventType string, startHeight uint64, endHeight uint64) ([]flow.BlockEvents, error) {
-	return c.handler.GetEventsForHeightRange(ctx, EventRangeQuery{
+	return c.Handler.GetEventsForHeightRange(ctx, EventRangeQuery{
 		Type:        eventType,
 		StartHeight: startHeight,
 		EndHeight:   endHeight,
@@ -88,13 +88,13 @@ func (c Client) GetEventsForHeightRange(ctx context.Context, eventType string, s
 }
 
 func (c Client) GetEventsForBlockIDs(ctx context.Context, eventType string, blockIDs []flow.Identifier) ([]flow.BlockEvents, error) {
-	return c.handler.GetEventsForBlockIDs(ctx, eventType, blockIDs)
+	return c.Handler.GetEventsForBlockIDs(ctx, eventType, blockIDs)
 }
 
 func (c Client) GetLatestProtocolStateSnapshot(ctx context.Context) ([]byte, error) {
-	return c.handler.GetLatestProtocolStateSnapshot(ctx)
+	return c.Handler.GetLatestProtocolStateSnapshot(ctx)
 }
 
 func (c Client) GetExecutionResultForBlockID(ctx context.Context, blockID flow.Identifier) (*flow.ExecutionResult, error) {
-	return c.handler.GetExecutionResultForBlockID(ctx, blockID)
+	return c.Handler.GetExecutionResultForBlockID(ctx, blockID)
 }
