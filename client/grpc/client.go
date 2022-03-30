e715bb37f2b2c90ab4ae82264b96f36d5b97ab6a
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-03-30 10:46:19 +0200

    rename client to base client

--- client/grpc/client.go
+++ client/grpc/client.go
@@ -37,17 +37,17 @@ const TESTNET_API = "access.devnet.nodes.onflow.org:9000"
 const CANARYNET_API = "access.canary.nodes.onflow.org:9000"
 const MAINNET_API = "access.mainnet.nodes.onflow.org:9000"
 
-func NewClient(handler *Handler) *Client {
-	return &Client{
+func NewClient(handler *Handler) *BaseClient {
+	return &BaseClient{
 		handler: handler,
 	}
 }
 
-type Client struct {
+type BaseClient struct {
 	handler *Handler
 }
 
-func NewDefaultEmulatorClient() (*Client, error) {
+func NewDefaultEmulatorClient() (*BaseClient, error) {
 	handler, err := New(EMULATOR_API)
 	if err != nil {
 		return nil, err
@@ -56,7 +56,7 @@ func NewDefaultEmulatorClient() (*Client, error) {
 	return NewClient(handler), nil
 }
 
-func NewDefaultTestnetClient() (*Client, error) {
+func NewDefaultTestnetClient() (*BaseClient, error) {
 	handler, err := New(TESTNET_API)
 	if err != nil {
 		return nil, err
@@ -65,7 +65,7 @@ func NewDefaultTestnetClient() (*Client, error) {
 	return NewClient(handler), nil
 }
 
-func NewDefaultCanaryClient() (*Client, error) {
+func NewDefaultCanaryClient() (*BaseClient, error) {
 	handler, err := New(CANARYNET_API)
 	if err != nil {
 		return nil, err
@@ -74,7 +74,7 @@ func NewDefaultCanaryClient() (*Client, error) {
 	return NewClient(handler), nil
 }
 
-func NewDefaultMainnetClient() (*Client, error) {
+func NewDefaultMainnetClient() (*BaseClient, error) {
 	handler, err := New(MAINNET_API)
 	if err != nil {
 		return nil, err
@@ -83,75 +83,75 @@ func NewDefaultMainnetClient() (*Client, error) {
 	return NewClient(handler), nil
 }
 
-func (c *Client) Ping(ctx context.Context) error {
+func (c *BaseClient) Ping(ctx context.Context) error {
 	return c.handler.Ping(ctx)
 }
 
-func (c *Client) GetLatestBlockHeader(ctx context.Context, isSealed bool) (*flow.BlockHeader, error) {
+func (c *BaseClient) GetLatestBlockHeader(ctx context.Context, isSealed bool) (*flow.BlockHeader, error) {
 	return c.handler.GetLatestBlockHeader(ctx, isSealed)
 }
 
-func (c *Client) GetBlockHeaderByID(ctx context.Context, blockID flow.Identifier) (*flow.BlockHeader, error) {
+func (c *BaseClient) GetBlockHeaderByID(ctx context.Context, blockID flow.Identifier) (*flow.BlockHeader, error) {
 	return c.handler.GetBlockHeaderByID(ctx, blockID)
 }
 
-func (c *Client) GetBlockHeaderByHeight(ctx context.Context, height uint64) (*flow.BlockHeader, error) {
+func (c *BaseClient) GetBlockHeaderByHeight(ctx context.Context, height uint64) (*flow.BlockHeader, error) {
 	return c.handler.GetBlockHeaderByHeight(ctx, height)
 }
 
-func (c *Client) GetLatestBlock(ctx context.Context, isSealed bool) (*flow.Block, error) {
+func (c *BaseClient) GetLatestBlock(ctx context.Context, isSealed bool) (*flow.Block, error) {
 	return c.handler.GetLatestBlock(ctx, isSealed)
 }
 
-func (c *Client) GetBlockByID(ctx context.Context, blockID flow.Identifier) (*flow.Block, error) {
+func (c *BaseClient) GetBlockByID(ctx context.Context, blockID flow.Identifier) (*flow.Block, error) {
 	return c.handler.GetBlockByID(ctx, blockID)
 }
 
-func (c *Client) GetBlockByHeight(ctx context.Context, height uint64) (*flow.Block, error) {
+func (c *BaseClient) GetBlockByHeight(ctx context.Context, height uint64) (*flow.Block, error) {
 	return c.handler.GetBlockByHeight(ctx, height)
 }
 
-func (c *Client) GetCollection(ctx context.Context, colID flow.Identifier) (*flow.Collection, error) {
+func (c *BaseClient) GetCollection(ctx context.Context, colID flow.Identifier) (*flow.Collection, error) {
 	return c.handler.GetCollection(ctx, colID)
 }
 
-func (c *Client) SendTransaction(ctx context.Context, tx flow.Transaction) error {
+func (c *BaseClient) SendTransaction(ctx context.Context, tx flow.Transaction) error {
 	return c.handler.SendTransaction(ctx, tx)
 }
 
-func (c *Client) GetTransaction(ctx context.Context, txID flow.Identifier) (*flow.Transaction, error) {
+func (c *BaseClient) GetTransaction(ctx context.Context, txID flow.Identifier) (*flow.Transaction, error) {
 	return c.handler.GetTransaction(ctx, txID)
 }
 
-func (c *Client) GetTransactionResult(ctx context.Context, txID flow.Identifier) (*flow.TransactionResult, error) {
+func (c *BaseClient) GetTransactionResult(ctx context.Context, txID flow.Identifier) (*flow.TransactionResult, error) {
 	return c.handler.GetTransactionResult(ctx, txID)
 }
 
-func (c *Client) GetAccount(ctx context.Context, address flow.Address) (*flow.Account, error) {
+func (c *BaseClient) GetAccount(ctx context.Context, address flow.Address) (*flow.Account, error) {
 	return c.handler.GetAccount(ctx, address)
 }
 
-func (c *Client) GetAccountAtLatestBlock(ctx context.Context, address flow.Address) (*flow.Account, error) {
+func (c *BaseClient) GetAccountAtLatestBlock(ctx context.Context, address flow.Address) (*flow.Account, error) {
 	return c.handler.GetAccountAtLatestBlock(ctx, address)
 }
 
-func (c *Client) GetAccountAtBlockHeight(ctx context.Context, address flow.Address, blockHeight uint64) (*flow.Account, error) {
+func (c *BaseClient) GetAccountAtBlockHeight(ctx context.Context, address flow.Address, blockHeight uint64) (*flow.Account, error) {
 	return c.handler.GetAccountAtBlockHeight(ctx, address, blockHeight)
 }
 
-func (c *Client) ExecuteScriptAtLatestBlock(ctx context.Context, script []byte, arguments []cadence.Value) (cadence.Value, error) {
+func (c *BaseClient) ExecuteScriptAtLatestBlock(ctx context.Context, script []byte, arguments []cadence.Value) (cadence.Value, error) {
 	return c.handler.ExecuteScriptAtLatestBlock(ctx, script, arguments)
 }
 
-func (c *Client) ExecuteScriptAtBlockID(ctx context.Context, blockID flow.Identifier, script []byte, arguments []cadence.Value) (cadence.Value, error) {
+func (c *BaseClient) ExecuteScriptAtBlockID(ctx context.Context, blockID flow.Identifier, script []byte, arguments []cadence.Value) (cadence.Value, error) {
 	return c.handler.ExecuteScriptAtBlockID(ctx, blockID, script, arguments)
 }
 
-func (c *Client) ExecuteScriptAtBlockHeight(ctx context.Context, height uint64, script []byte, arguments []cadence.Value) (cadence.Value, error) {
+func (c *BaseClient) ExecuteScriptAtBlockHeight(ctx context.Context, height uint64, script []byte, arguments []cadence.Value) (cadence.Value, error) {
 	return c.handler.ExecuteScriptAtBlockHeight(ctx, height, script, arguments)
 }
 
-func (c *Client) GetEventsForHeightRange(ctx context.Context, eventType string, startHeight uint64, endHeight uint64) ([]flow.BlockEvents, error) {
+func (c *BaseClient) GetEventsForHeightRange(ctx context.Context, eventType string, startHeight uint64, endHeight uint64) ([]flow.BlockEvents, error) {
 	return c.handler.GetEventsForHeightRange(ctx, EventRangeQuery{
 		Type:        eventType,
 		StartHeight: startHeight,
@@ -159,14 +159,14 @@ func (c *Client) GetEventsForHeightRange(ctx context.Context, eventType string, 
 	})
 }
 
-func (c *Client) GetEventsForBlockIDs(ctx context.Context, eventType string, blockIDs []flow.Identifier) ([]flow.BlockEvents, error) {
+func (c *BaseClient) GetEventsForBlockIDs(ctx context.Context, eventType string, blockIDs []flow.Identifier) ([]flow.BlockEvents, error) {
 	return c.handler.GetEventsForBlockIDs(ctx, eventType, blockIDs)
 }
 
-func (c *Client) GetLatestProtocolStateSnapshot(ctx context.Context) ([]byte, error) {
+func (c *BaseClient) GetLatestProtocolStateSnapshot(ctx context.Context) ([]byte, error) {
 	return c.handler.GetLatestProtocolStateSnapshot(ctx)
 }
 
-func (c *Client) GetExecutionResultForBlockID(ctx context.Context, blockID flow.Identifier) (*flow.ExecutionResult, error) {
+func (c *BaseClient) GetExecutionResultForBlockID(ctx context.Context, blockID flow.Identifier) (*flow.ExecutionResult, error) {
 	return c.handler.GetExecutionResultForBlockID(ctx, blockID)
 }
