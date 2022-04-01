2d180406b20b25bcb80c5562b7e47f589a32306f
Bruno Bachmann <bfbachmann@gmail.com>
2022-04-01 10:37:33 -0700

    Add updated account proof encoding function

--- account_proof.go
+++ account_proof.go
@@ -19,10 +19,27 @@
 package flow
 
 import (
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/rlp"
 )
+
+// AccountProofNonceMinLenBytes is the minimum length of account proof nonces in bytes.
+const AccountProofNonceMinLenBytes = 32
+
+var (
+	// ErrInvalidNonce is returned when the account proof nonce passed to a function is invalid.
+	ErrInvalidNonce = errors.New("invalid nonce")
+)
+
+type canonicalAccountProofV2 struct {
+	AppID   string
+	Address []byte
+	Nonce   []byte
+}
 
 type canonicalAccountProofWithoutTag struct {
 	Address   []byte
@@ -37,6 +54,8 @@ type canonicalAccountProofWithTag struct {
 
 // NewAccountProofMessage creates a new account proof message for singing. The appDomainTag is optional and can be left
 // empty. Note that the resulting byte slice does not contain the user domain tag.
+// Deprecated: This function implements an old version of the account proof encoding scheme. Use
+// NewAccountProofMessageV2 for the latest version.
 func NewAccountProofMessage(address Address, timestamp int64, appDomainTag string) ([]byte, error) {
 	var (
 		encodedMessage []byte
@@ -66,4 +85,28 @@ func NewAccountProofMessage(address Address, timestamp int64, appDomainTag strin
 	}
 
 	return encodedMessage, nil
+}
+
+// NewAccountProofMessageV2 creates a new account proof message for singing. The appID is optional and can be left
+// empty. The encoded message returned does not include the user domain tag.
+func NewAccountProofMessageV2(address Address, appID, nonceHex string) ([]byte, error) {
+	nonceBytes, err := hex.DecodeString(strings.TrimPrefix(nonceHex, "0x"))
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrInvalidNonce, err)
+	}
+
+	if len(nonceBytes) < AccountProofNonceMinLenBytes {
+		return nil, fmt.Errorf("%w: nonce must be at least %d bytes", ErrInvalidNonce, AccountProofNonceMinLenBytes)
+	}
+
+	msg, err := rlp.EncodeToBytes(&canonicalAccountProofV2{
+		AppID:   appID,
+		Address: address.Bytes(),
+		Nonce:   nonceBytes,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error encoding account proof message: %w", err)
+	}
+
+	return msg, nil
 }
--- account_proof_test.go
+++ account_proof_test.go
@@ -20,6 +20,7 @@ package flow
 
 import (
 	"encoding/hex"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -51,6 +52,54 @@ func TestNewAccountProofMsg(t *testing.T) {
 			msg, err := NewAccountProofMessage(tc.address, tc.timestamp, tc.appDomainTag)
 			assert.NoError(t, err)
 			assert.Equal(t, tc.expectedResult, hex.EncodeToString(msg))
+		})
+	}
+}
+
+func TestNewAccountProofMessageV2(t *testing.T) {
+	type testCase struct {
+		address        Address
+		nonce          string
+		appID          string
+		expectedResult string
+		expectedErr    error
+	}
+
+	for name, tc := range map[string]testCase{
+		"valid inputs": {
+			address: HexToAddress("ABC123DEF456"),
+			// nolint: lll
+			nonce: "3037366134636339643564623330316636626239323161663465346131393662",
+			appID: "AWESOME-APP-ID",
+			// nolint: lll
+			expectedResult: "f8398e415745534f4d452d4150502d4944880000abc123def456a03037366134636339643564623330316636626239323161663465346131393662",
+		},
+		"nonce invalid hex": {
+			address: HexToAddress("ABC123DEF456"),
+			// nolint: lll
+			nonce: "asdf",
+			appID: "AWESOME-APP-ID",
+			// nolint: lll
+			expectedErr: ErrInvalidNonce,
+		},
+		"nonce too short": {
+			address: HexToAddress("ABC123DEF456"),
+			// nolint: lll
+			nonce: "222222",
+			appID: "AWESOME-APP-ID",
+			// nolint: lll
+			expectedErr: ErrInvalidNonce,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			// Check the output of NewAccountProofMessage against a pre-generated message from the flow-js-sdk
+			msg, err := NewAccountProofMessageV2(tc.address, tc.appID, tc.nonce)
+			if tc.expectedErr != nil {
+				assert.True(t, errors.Is(err, tc.expectedErr))
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedResult, hex.EncodeToString(msg))
+			}
 		})
 	}
 }
